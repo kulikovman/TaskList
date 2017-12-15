e99b7a3bdc3d1987446d1e330dd3dab3f2e4c132
Maksim Kulikov <kulikovman@gmail.com>
2017-12-15 19:54:50 +0500

    Обертка в диалог с интерфейсом

--- app/src/main/java/ru/kulikovman/tasklist/dialogs/PriorityDialog.java
+++ app/src/main/java/ru/kulikovman/tasklist/dialogs/PriorityDialog.java
@@ -1,19 +1,37 @@
 package ru.kulikovman.tasklist.dialogs;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 
 import io.realm.Realm;
+import ru.kulikovman.tasklist.CallbackDialogFragment;
 import ru.kulikovman.tasklist.R;
 import ru.kulikovman.tasklist.models.Task;
 
-public class PriorityDialog extends DialogFragment {
+public class PriorityDialog extends CallbackDialogFragment {
     private Realm mRealm;
     private Task mTask;
 
+    CallbackDialogListener mListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (CallbackDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(context.toString() + " must implement CallbackDialogListener");
+        }
+    }
+
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Получаем аргументы
