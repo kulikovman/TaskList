3b178ab65eb3ceb6a91f9a95e3019112c24929fe
Maksim Kulikov <kulikovman@gmail.com>
2017-12-16 19:26:09 +0500

    Ссылка на группы через боковое меню

--- app/src/main/java/ru/kulikovman/tasklist/TaskListActivity.java
+++ app/src/main/java/ru/kulikovman/tasklist/TaskListActivity.java
@@ -321,18 +321,14 @@ public class TaskListActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-            DialogFragment infoDialog = new InfoDialog();
-            infoDialog.show(getSupportFragmentManager(), "infoDialog");
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_all_task) {
 
-        } else if (id == R.id.nav_manage) {
-
+        } else if (id == R.id.nav_group_list) {
+            Intent intent = new Intent(this, GroupListActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_fghdfhg) {
 
         }
 
